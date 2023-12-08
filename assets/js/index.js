d744b3b535c4a307d0bdc1835d55666a76c842c2
Kaua2123 <kauantnetsoares@gmail.com>
2023-12-08 08:56:07 -0300

    Validação
    -> Construindo classe para validar CPF.

--- assets/js/index.js
+++ assets/js/index.js
@@ -77,6 +77,112 @@ class ValidaFormulario {
 
 }   
 
+class validaCpf {
+    constructor(cpf) {
+        this.cpf = cpf;
+        this.cpfArray = [];
+        this.cpfNovo = "";
+        this.totalDigito1 = 0;
+        this.totalDigito2 = 0;
+        this.primeiroDigito = 0;
+        this.segundoDigito = 0;
+    }
 
+    //Método responsável por tirar os pontos do cpf
+    limpaCpf() {
+        this.cpfLimpo = this.cpf.replace(/\D+/g, '');
+        return this.cpfLimpo;
+    }
+    
+    encontraPrimeiroDigito() {
+        this.cpfLimpo = this.limpaCpf(this.cpf);
+        this.cpfArray = Array.from(this.cpfLimpo);
+
+        this.totalDigito1 = 0;
+        
+        //multiplicação regressiva do cpf
+        for (let i=0; i<=this.cpfArray.length; i++){
+
+            if (i == this.cpfArray[0]) this.totalDigito1  += parseInt((i * 10));
+            if (i == this.cpfArray[1]) this.totalDigito1 += parseInt(i * 9);
+            if (i == this.cpfArray[2]) this.totalDigito1 += parseInt(i * 8);
+            if (i == this.cpfArray[3]) this.totalDigito1 += parseInt(i * 7);
+            if (i == this.cpfArray[4]) this.totalDigito1 += parseInt(i * 6); 
+            if (i == this.cpfArray[5]) this.totalDigito1 += parseInt(i * 5); 
+            if (i == this.cpfArray[6]) this.totalDigito1 += parseInt(i * 4);
+            if (i == this.cpfArray[7]) this.totalDigito1 += parseInt(i * 3);
+            if (i == this.cpfArray[8]) this.totalDigito1 += parseInt(i * 2);
+         
+        }
+
+        //VERIFICAÇÃO 
+        this.primeiroDigito = 11 - (this.totalDigito1 % 11);
+        this.primeiroDigito >= 10 ? this.primeiroDigito = '0' : this.primeiroDigito;
+
+        return this.primeiroDigito;
+    }
+
+    encontraSegundoDigito() {
+
+        this.cpfLimpo = this.limpaCpf(this.cpf);
+        this.cpfArray = Array.from(this.cpfLimpo);
+        this.totalDigito2 = 0;
+        this.primeiroDigito = this.encontraPrimeiroDigito(this.cpf);
+
+
+        //multiplicação regressiva do cpf
+        for (let i=0; i<=this.cpfArray.length; i++){
+
+            if (i == this.cpfArray[0]) this.totalDigito1 += parseInt((i * 11));
+            if (i == this.cpfArray[1]) this.totalDigito1 += parseInt(i * 10);
+            if (i == this.cpfArray[2]) this.totalDigito1 += parseInt(i * 9);
+            if (i == this.cpfArray[3]) this.totalDigito1 += parseInt(i * 8);
+            if (i == this.cpfArray[4]) this.totalDigito1 += parseInt(i * 7); 
+            if (i == this.cpfArray[5]) this.totalDigito1 += parseInt(i * 6); 
+            if (i == this.cpfArray[6]) this.totalDigito1 += parseInt(i * 5);
+            if (i == this.cpfArray[7]) this.totalDigito1 += parseInt(i * 4);
+            if (i == this.cpfArray[8]) this.totalDigito1 += parseInt(i * 3);
+         
+        }
+        this.totalDigito2 += this.primeiroDigito * 2;
+        
+        //VERIFICAÇÃO 
+        this.segundoDigito = 11 - (this.totalDigito2 % 11);
+        this.segundoDigito >= 10 ? this.segundoDigito = '0' : this.segundoDigito;
+
+        return this.segundoDigito;
+    }
+    
+    validandoCpf(cpf) {
+        
+        this.cpf = cpf;
+        this.limpaCpf(this.cpf)
+        this.encontraPrimeiroDigito(this.cpf);
+        this.encontraSegundoDigito(this.cpf);
+        
+        this.cpfLimpo = this.limpaCpf(this.cpf);
+        this.cpfArray = Array.from(this.cpfLimpo);
+        this.primeiroDigito = this.encontraPrimeiroDigito(this.cpf);
+        this.segundoDigito = this.encontraSegundoDigito(this.cpf);
+
+        console.log(this.primeiroDigito, this.segundoDigito);
+
+        this.cpfArraySliced = this.cpfArray.slice(0, -2);
+        
+
+        if(this.cpfArray.length != 11 || this.primeiroDigito != this.cpfArray[9] || this.segundoDigito != this.cpfArray[10]){
+            console.log('cpf inválido!')
+            return 'CPF Inválido!';
+        } 
+        else {
+            console.log('cpf válido!')
+            return;
+        }
+        
+    }
+
+}
 
 const formulario = new ValidaFormulario();
+const cpf = new validaCpf();
+cpf.validandoCpf('') // passar cpf como parâmetro para o método
